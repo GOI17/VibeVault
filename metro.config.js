1c41baefe04f2c738b438bde6de9ae7277cb4608
Gilberto Olivas <olivasgilberto@gmail.com>
2025-10-01 21:31:10 -0600

    gh pages fix

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-config.transformer.publicPath = "/VibeVault/";
-
-module.exports = config;
