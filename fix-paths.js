343c2e3c1e4bdffa52f2829bdf4a7ea84d33067b
Gilberto Olivas <olivasgilberto@gmail.com>
2025-10-04 20:11:34 -0600

    one more from sonnet

--- fix-paths.js
+++ fix-paths.js
@@ -36,6 +36,45 @@ function fixPaths(dir, extensions) {
   });
 }
 
+// Add redirect script to index.html
+function addRedirectScript() {
+  const indexPath = path.join(__dirname, "dist", "index.html");
+  let html = fs.readFileSync(indexPath, "utf8");
+
+  const redirectScript = `
+    <script>
+      (function(l) {
+        if (l.search[1] === '/' ) {
+          var decoded = l.search.slice(1).split('&').map(function(s) {
+            return s.replace(/~and~/g, '&')
+          }).join('?');
+          window.history.replaceState(null, null,
+            l.pathname.slice(0, -1) + decoded + l.hash
+          );
+        }
+      }(window.location))
+    </script>`;
+
+  // Insert before closing </head>
+  html = html.replace("</head>", `${redirectScript}\n</head>`);
+  fs.writeFileSync(indexPath, html);
+  console.log("Added redirect script to index.html");
+}
+
+// Copy 404.html if it exists in public folder
+function copy404() {
+  const source = path.join(__dirname, "public", "404.html");
+  const dest = path.join(__dirname, "dist", "404.html");
+
+  if (fs.existsSync(source)) {
+    let html = fs.readFileSync(source, "utf8");
+    fs.writeFileSync(dest, html);
+    console.log("Copied 404.html to dist");
+  }
+}
+
 const distPath = path.join(__dirname, "dist");
 fixPaths(distPath, [".html", ".js", ".css"]);
+addRedirectScript();
+copy404();
 console.log("All files fixed!");
