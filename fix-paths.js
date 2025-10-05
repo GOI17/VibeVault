6cee035988fbb40946b1352294b283b77a809206
Gilberto Olivas <olivasgilberto@gmail.com>
2025-10-04 20:00:25 -0600

    sonnet is on fire

--- fix-paths.js
+++ fix-paths.js
@@ -1,12 +1,41 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const distPath = path.join(__dirname, 'dist');
-const indexPath = path.join(distPath, 'index.html');
+function fixPaths(dir, extensions) {
+  const files = fs.readdirSync(dir);
 
-let html = fs.readFileSync(indexPath, 'utf8');
-html = html.replace(/src="\//g, 'src="/VibeVault/');
-html = html.replace(/href="\//g, 'href="/VibeVault/');
-fs.writeFileSync(indexPath, html);
+  files.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
 
-console.log('Fixed asset paths');
+    if (stat.isDirectory()) {
+      fixPaths(filePath, extensions);
+    } else if (extensions.some((ext) => file.endsWith(ext))) {
+      let content = fs.readFileSync(filePath, "utf8");
+      const original = content;
+
+      // Fix HTML attributes
+      content = content.replace(/src="\//g, 'src="/VibeVault/');
+      content = content.replace(/href="\//g, 'href="/VibeVault/');
+      content = content.replace(/content="\//g, 'content="/VibeVault/');
+
+      // Fix url() in CSS/JS
+      content = content.replace(/url\(\//g, "url(/VibeVault/");
+      content = content.replace(/url\("\//g, 'url("/VibeVault/');
+      content = content.replace(/url\('\//g, "url('/VibeVault/");
+
+      // Fix standalone paths
+      content = content.replace(/"\/(_expo|assets)\//g, '"/VibeVault/$1/');
+      content = content.replace(/'\/(_expo|assets)\//g, "'/VibeVault/$1/");
+
+      if (content !== original) {
+        fs.writeFileSync(filePath, content);
+        console.log(`Fixed: ${filePath}`);
+      }
+    }
+  });
+}
+
+const distPath = path.join(__dirname, "dist");
+fixPaths(distPath, [".html", ".js", ".css"]);
+console.log("All files fixed!");
