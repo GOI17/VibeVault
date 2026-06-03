58e4ff29fbf3134bc1c07f883afbf740f8e33fee
Gilberto Olivas <olivasgilberto@gmail.com>
2026-06-02 21:18:27 -0600

    fix(details): restore details page scrolling

--- app/home/details/[id].tsx
+++ app/home/details/[id].tsx
@@ -1,11 +1,16 @@
-import { useRoute, type RouteProp } from "@react-navigation/native";
-import type { ReactElement } from "react";
+import { useNavigation, useRoute, type NavigationProp, type RouteProp } from "@react-navigation/native";
+import { useLayoutEffect, type ReactElement } from "react";
 
 import { DetailsContainer } from "@/containers/DetailsContainer";
 import type { RootStackParamList } from "@/app/navigation/types";
 
 export default function DetailsScreen(): ReactElement {
   const route = useRoute<RouteProp<RootStackParamList, "Details">>();
+  const navigation = useNavigation<NavigationProp<RootStackParamList, "Details">>();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerMode: "float" });
+  }, [navigation]);
 
   return <DetailsContainer params={route.params} />;
 }
