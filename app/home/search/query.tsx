696db9eb6c132f56d5d0b5809308d65a543cbdcd
Gilberto Olivas <olivasgilberto@gmail.com>
2026-06-07 14:30:50 -0600

    fix(search): keep results scroll inside route

--- app/home/search/query.tsx
+++ app/home/search/query.tsx
@@ -1,12 +1,17 @@
-import { useRoute, type RouteProp } from "@react-navigation/native";
-import { type ReactElement } from "react";
+import { useNavigation, useRoute, type NavigationProp, type RouteProp } from "@react-navigation/native";
+import { useLayoutEffect, type ReactElement } from "react";
 
 import type { RootStackParamList } from "@/app/navigation/types";
 import { SearchContainer } from "@/containers/SearchContainer";
 
 export default function SearchScreen(): ReactElement {
   const route = useRoute<RouteProp<RootStackParamList, "Search">>();
+  const navigation = useNavigation<NavigationProp<RootStackParamList, "Search">>();
   const query = route.params?.query ?? "";
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerMode: "float" });
+  }, [navigation]);
 
   return <SearchContainer query={query} />;
 }
